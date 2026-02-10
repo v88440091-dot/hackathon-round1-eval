8de256eafc193023b42212042699e8c29031d65b
v88440091-dot <v88440091@gmail.com>
2026-02-10 05:50:15 +0530

    batch-1

--- solution.py
+++ solution.py
@@ -1,12 +1,2 @@
-# Write your solution here
-# Do not change the file name
-
-# Input:
-# Two space-separated integers
-
-# Output:
-# Print their sum
-
-# Example:
-# a, b = map(int, input().split())
-# print(a + b)
+a, b = map(int, input().split())
+print(a + b)
